13c4312cbda37fe36e6222583b933c25483d4e6f
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2026-04-19 00:53:13 +0530

    feat: add video_props type and default_video_props constant

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type video_props = {
+  title: string;
+  scenes: string[];
+  image: string;
+  siteName: string;
+};
+
+export const default_video_props: video_props = {
+  title: "How to Turn a Blog Post into a Short Video",
+  scenes: [
+    "Use one strong hook in the first 3 seconds.",
+    "Break the idea into 2 or 3 simple points.",
+    "End with a clear call to action to read more.",
+  ],
+  image: "https://picsum.photos/1080/1920",
+  siteName: "Remotion API",
+};
