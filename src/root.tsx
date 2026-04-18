017cf2c0942a635547a514d3b7c346820b6a10b8
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2026-04-19 00:57:15 +0530

    feat: add remotion_root component with video composition setup

---
+++ src/root.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Composition } from "remotion";
+import { default_video_props } from "./types";
+import { VideoComposition } from "./video";
+
+export const remotion_root: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="post-video"
+        component={VideoComposition}
+        durationInFrames={750}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={default_video_props}
+      />
+    </>
+  );
+};
